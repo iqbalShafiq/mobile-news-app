ee69119385cc10c888bde553a4bc9a569e6adae0
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2023-09-30 00:47:16 +0700

    feat(Dependency Injection): init application name
    - create new application class to hold modules from dependency injection in the next process

---
+++ app/src/main/java/space/iqbalsyafiq/mobilenewsapp/MyApplication.kt
@@ -0,0 +1,17 @@
+package space.iqbalsyafiq.mobilenewsapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+        }
+    }
+}
