698883b9990921afe1a155e44b6bb1873ea80619
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2023-09-30 01:13:14 +0700

    feat(Splash Screen): set up splash screen
    - create the layout of splash screen in the launcher screen
    - give 2 second delay before move to the next screen

--- app/src/main/java/space/iqbalsyafiq/mobilenewsapp/ui/MainActivity.kt
+++ app/src/main/java/space/iqbalsyafiq/mobilenewsapp/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package space.iqbalsyafiq.mobilenewsapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import space.iqbalsyafiq.mobilenewsapp.R
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import space.iqbalsyafiq.mobilenewsapp.databinding.ActivityMainBinding
+import space.iqbalsyafiq.mobilenewsapp.ui.category_list.CategoryListActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // give delay to the splash screen before move to category list screen
+        lifecycleScope.launch {
+            delay(2000)
+            Intent(
+                this@MainActivity,
+                CategoryListActivity::class.java
+            ).apply {
+                startActivity(this)
+            }
+        }
     }
 }
